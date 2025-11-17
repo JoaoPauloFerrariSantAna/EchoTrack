aefe20b69e76025f4f1f90d7ce207a7235c2abc7
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-11-17 14:26:34 -0300

    feat: implemented new attributes to repo and new method

--- Repositories/HorseRepository.cs
+++ Repositories/HorseRepository.cs
@@ -1,10 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EchoTrackV2.Interfaces;
+using NuGet.Packaging.Signing;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace EchoTrackV2.Repositories;
 
-public class HorseRepository
+public class HorseRepository :  IAnimalRepository
 {
+    // TODO: add context
+
     [Editable(false)]
     public int Id { get; set; }
 
@@ -12,4 +16,36 @@ public class HorseRepository
     [DisallowNull]
     [Editable(true)]
     public string Name { get; set; }
+
+    [DisallowNull]
+    [Editable(false)]
+    public uint AmountEaten { get; private set; } = 0U;
+
+    [DisallowNull]
+    [Editable(false)]
+    public double MaxAmountToEat { get; private set; } = 11.75;
+
+    [DisallowNull]
+    [Editable(false)]
+    public bool IsFull { get; private set; } = false;
+
+    public bool Eat(uint amountToFeed)
+    {
+        // it can't eat anymore
+        if (IsFull) return false;
+
+        // it can't eat negative values
+        if (amountToFeed <= 0) return false;
+
+        AmountEaten += amountToFeed;
+
+        if (AmountEaten >= MaxAmountToEat) IsFull = true;
+
+        return true;
+    }
+
+    public void Defecate()
+    {
+        throw new NotImplementedException();
+    }
 }
